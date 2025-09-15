477f1e3dfe68f13d050a6d1c3b4f6d36cfd12bb0
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-09-16 00:18:03 +0530

    LandingPage added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <>
+    <div className='h-full w-full'>
       <LandingPage />
-    </>
+    </div>
   )
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 
 const LandingPage = () => {
   return (
-    <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
+    <section className="w-full px-1 py-32 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
       <div>
         <span className="block mb-4 text-xs md:text-sm text-indigo-500 font-medium">
           Better every day
@@ -13,11 +13,10 @@ const LandingPage = () => {
           Let's change it up a bit
         </h3>
         <p className="text-base md:text-lg text-slate-700 my-4 md:my-6">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam nobis in
-          error repellat voluptatibus ad.
+            Discover a new way to stay active and motivated with our dynamic through for online therapy classes. Our platform offers a variety of engaging and interactive sessions led by experienced instructors, designed to fit your schedule and fitness level. Whether you're looking to improve your strength, flexibility, or overall well-being, our online therapy classes provide the perfect solution. Join us today and take the first step towards a healthier, happier you!
         </p>
         <button className="bg-indigo-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-indigo-600 active:scale-95">
-          Find a class
+          Find a center nearby
         </button>
       </div>
       <ShuffleGrid />
