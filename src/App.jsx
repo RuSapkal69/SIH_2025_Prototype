557813b86384173bde9cf8fa3e6e85c51bf01be1
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-09-15 23:55:31 +0530

    LandingPage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-
+import LandingPage from './pages/LandingPage'
 import React from 'react'
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen text-3xl font-bold">
-      Starting Soon, 
-    </div>
+    <>
+      <LandingPage />
+    </>
   )
 }
 
