bdbd71edaca5b382059a678903dc658461470801
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-09-15 23:28:39 +0530

    Let's start working asap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-3xl font-bold">
+      Starting Soon, 
+    </div>
+  )
+}
+
+export default App
